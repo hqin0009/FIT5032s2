08bb2a67e4111901a44c5a8f8c219ba53644e057
Frank <hqin0009@student.monash.edu>
2020-09-18 18:36:28 +1000

    Week7

---
+++ Week7/SQLQuery1.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[Students](
+	[Id] int identity(1,1) Not Null,
+	[FirstName] nvarchar(max) Not Null,
+	[LastName] nvarchar(max) Not Null,
+	[UserId] nvarchar(max) Not Null,
+	PRIMARY KEY (Id)
+	);
+	GO
+	
+	CREATE TABLE [dbo].[Units](
+	[Id] int identity(1,1) Not Null,
+	[Name] nvarchar(max) Not Null,
+	[Description] nvarchar(max) Not Null,
+	[StudentId] int Not Null,
+	PRIMARY KEY (Id),
+	FOREIGN KEY (StudentId) REFERENCES Students (Id)
+	);
+	GO
